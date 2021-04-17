986ef75a024008030f9511b5ef941689119e2064
Ashish Hora <t821334@spark.co.nz>
2021-04-18 07:05:59 +1200

    home page edits and social links changes

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Twitter from "../assets/twitter-alt.svg";
 import GitHub from "../assets/github-alt.svg";
+import Pin from "../assets/pinterest-alt.svg";
 import config from "../lib/config";
 
 export function SocialList({}) {
   return (
     <div>
       <a
-        title="Twitter"
+        title="@hsihsa06"
         href={`https://twitter.com/${config.twitter_account}`}
         target="_blank"
         rel="noopener"
@@ -15,12 +16,12 @@ export function SocialList({}) {
         <Twitter width={24} height={24} fill={"#222"} />
       </a>
       <a
-        title="GitHub"
+        title="Pinterest"
         href={`https://github.com/${config.github_account}`}
         target="_blank"
         rel="noopener"
       >
-        <GitHub width={24} height={24} fill={"#222"} />
+        <Pin width={24} height={24} fill={"#222"} />
       </a>
       <style jsx>{`
         a {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,9 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, My name is Reena<span className="fancy"></span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2 className="fancy">I love to cook and sharing some of my recipes on this blog</h2>
           <SocialList />
         </div>
       </div>
