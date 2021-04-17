676d961a8ec393e64e60d02286862893ac37dc29
Ashish Hora <t821334@spark.co.nz>
2021-04-18 06:26:59 +1200

    mock detail recipe page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -23,7 +23,7 @@ export default function Navigation() {
                   router.pathname.startsWith("/posts") ? "active" : null
                 }
               >
-                blog
+                recipes
               </a>
             </Link>
           </li>
